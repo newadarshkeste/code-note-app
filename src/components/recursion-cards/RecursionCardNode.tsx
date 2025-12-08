60a1e7308eab8dc09af781d15b076f7d1f56c6ee
Adarsh Keste <adarshkeste.job2025@gmail.com>
2025-12-08 15:40:46 +0000

    now I am unable to select from which node I should select and which to c

--- src/components/recursion-cards/RecursionCardNode.tsx
+++ src/components/recursion-cards/RecursionCardNode.tsx
@@ -51,13 +51,9 @@ export function RecursionCardNode({ data, selected }: NodeProps<RecursionCard>) 
             style={{ width: data.width ? `${data.width}px` : '256px' }}
         >
             <Handle type="target" position={Position.Top} className="!bg-primary" />
-            <Handle type="source" position={Position.Top} className="!bg-primary" />
-            <Handle type="target" position={Position.Right} className="!bg-primary" />
-            <Handle type="source" position={Position.Right} className="!bg-primary" />
-            <Handle type="target" position={Position.Bottom} className="!bg-primary" />
             <Handle type="source" position={Position.Bottom} className="!bg-primary" />
             <Handle type="target" position={Position.Left} className="!bg-primary" />
-            <Handle type="source" position={Position.Left} className="!bg-primary" />
+            <Handle type="source" position={Position.Right} className="!bg-primary" />
             
             <div className="p-3">
                 <div className="flex justify-between items-start gap-2">
