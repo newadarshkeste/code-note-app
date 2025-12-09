c2e022c665bd61b1e1e3d16e423a33e11432838f
Adarsh Keste <adarshkeste.job2025@gmail.com>
2025-12-09 08:19:53 +0000

    Unhandled Runtime Error TypeError: Cannot read properties of undefined (

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -50,6 +50,7 @@ import {
     verticalListSortingStrategy,
     useSortable,
 } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible';
 import { useToast } from '@/hooks/use-toast';
 
